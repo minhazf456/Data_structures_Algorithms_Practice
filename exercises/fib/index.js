ea25b283229ae313072a38059473745fe83624bc
minhazf456 <minhazf@gmail.com>
2020-01-20 22:08:27 -0800

    updated stacks and queue

---
+++ exercises/fib/index.js
@@ -0,0 +1,37 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+// iterative solution
+
+function fib(n) {
+    const result = [0, 1]; /// we are gonna declare an array which gonna hold a series of numbers.
+
+    for (let i =2; i <=n; i++) {
+        const a = result [i -1];
+        const b = result [i -2];
+        result.push(a +b);
+    }
+    return result [n];
+}
+
+module.exports = fib;
+
+/// recursive solution
+
+function fib(n) {
+    if (n <2){
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
+// SO runtime analysis is exponential time 2^n
+
+
