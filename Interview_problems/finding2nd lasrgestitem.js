8b676f63a4cdb450cde57acb67d19d7bec229cbe
minhazf456 <minhazf@gmail.com>
2020-02-16 21:55:10 -0800

    solved recursive on BST

--- Interview_problems/finding2nd lasrgestitem.js
+++ Interview_problems/finding2nd lasrgestitem.js
@@ -25,6 +25,28 @@ class BinaryTreeNode {
           throw new Error("Tree must have at least one node")
       }
       if (rootNode.right) {
-          return findLargest(rootNode,)
+          return findLargest(rootNode.right);
       }
+      return rootNode.value:
   }
+
+  function findSecondLargest(rootNode){
+      if (!rootNode || (!rootNode.left && !rootNode.right)) {
+          throw new Error ("Tree must have at least 2 nodes")
+      }
+// case: now we are currently at the largest element, and largest has a left subtree,
+// so second largest is largest is said subtree
+if (rootNode.left && !rootNode.right){
+    return findLargest(rootNode.left);
+}
+
+// Case: we are at the largest, and largest has no left subtree
+// so 2nd largst is largest in said subtree
+
+if (rootNode.right &&  ! rootNode.right.left && ! rootNode.right.right){
+    return rootNode.value;
+}
+
+// otherwise step right 
+return findSecondLargest(rootNode.right)
+}
