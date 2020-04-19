8a96806feec843bb3074f209f27386504a184aec
minhazf456 <minhazf@gmail.com>
2020-04-19 05:20:07 -0700

    Solved AppleStockPriblems

---
+++ Interview_problems/AppleStock.js
@@ -0,0 +1,7 @@
+
+// Write an efficient function that takes stockPrices and returns the best profit 
+// I could have made from one purchase and one sale of one share of Apple stock 
+// yesterday.
+
+
+
