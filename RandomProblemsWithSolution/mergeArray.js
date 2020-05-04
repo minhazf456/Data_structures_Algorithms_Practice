49d585d91afbfc32f1db7a74c9473f212426a9ca
minhazf456 <minhazf@gmail.com>
2020-05-03 17:25:16 -0700

    optimized version of checking orders

--- RandomProblemsWithSolution/mergeArray.js
+++ RandomProblemsWithSolution/mergeArray.js
@@ -1,22 +1,31 @@
-function mergeArrays(myArray, alicesArray){
-    const mergedArray=[];
-    let currentIndexALices= 0;
-    let CurrentIndexMine =0;
+function mergeArrays(myArray, alicesArray) {
+    // Set up our mergedArray
+    const mergedArray = [];
+    let currentIndexAlices = 0;
+    let currentIndexMine = 0;
     let currentIndexMerged = 0;
-
-    while(currentIndexMerged <(myArray.length + alicesArray.length)){
-        const isMyArrayExhausted = CurrentIndexMyArray >= myArray.length;
-        const isAlicesArrayExhausted= currentIndexALices>= alicesArray.length;
-
-        if (!isMyArrayExhausted && (isAlicesArrayExhausted || (myArray[CurrentIndexMine]< alicesArray[currentIndexALices]))){
-            mergedArray[currentIndexMerged]= myArray[CurrentIndexMine];
-            CurrentIndexMine++;
-        }
-        else {
-            mergedArray[currentIndexMerged] = alicesArray[currentIndexALices];
-            currentIndexALices++
-        }
-        currentIndexMerged++;
+  
+    while (currentIndexMerged < (myArray.length + alicesArray.length)) {
+      const isMyArrayExhausted = currentIndexMine >= myArray.length;
+      const isAlicesArrayExhausted = currentIndexAlices >= alicesArray.length;
+      // Case: next comes from my array
+      // My array must not be exhausted, and EITHER:
+      // 1) Alice's array IS exhausted, or
+      // 2) The current element in my array is less
+      //    than the current element in Alice's array
+      if (!isMyArrayExhausted && (isAlicesArrayExhausted ||
+        (myArray[currentIndexMine] < alicesArray[currentIndexAlices]))) {
+  
+        mergedArray[currentIndexMerged] = myArray[currentIndexMine];
+        currentIndexMine++;
+  
+        // Case: next comes from Alice's array
+      } else {
+        mergedArray[currentIndexMerged] = alicesArray[currentIndexAlices];
+        currentIndexAlices++;
+      }
+      currentIndexMerged++;
     }
-        return mergedArray;
-    }
+    return mergedArray;
+  }
+  
